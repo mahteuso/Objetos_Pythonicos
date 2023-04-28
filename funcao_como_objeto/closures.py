7d26529ddf998b6bccfde584c98a1022b9f310dc
mateus <mateus@ufscar.br>
2023-04-28 05:56:57 -0300

    aprendendo sobre funções de ordem superiores

--- funcao_como_objeto/closures.py
+++ funcao_como_objeto/closures.py
@@ -1,10 +1,18 @@
-def externa(func):
-    def interna():
-        return func
+def decorador(func):
+    print(func.__name__)
+    def interna(x, y):
+        if isinstance(x, int) and isinstance(y, int):
+            return func(x, y)
+        else:
+            raise ValueError('Entre somente com números')
     return interna
 
+@decorador
+def interna(x, y):
+    return x + y
 
-@externa
-def desenha():
-    print("Desenhando")
+print(interna(2, "Mateus"))
+
+
+
 
